8b3f7ad3aaba108f5b93add56e4b902c8fb28fa5
Talha Haider <talhahaider074@gmail.com>
2025-11-27 21:27:57 +0500

    datre time changes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -222,10 +222,9 @@ const LandingPage = () => {
   const wrapperRef = useRef(null);
   const [location, setLocation] = useState('');
   const [fromDate, setFromDate] = useState('');
-  const [fromTime, setFromTime] = useState('');
+  const [fromTime, setFromTime] = useState("10:00");
   const [untilDate, setUntilDate] = useState('');
-  const [untilTime, setUntilTime] = useState('');
-
+  const [untilTime, setUntilTime] = useState("22:00");
   const [suggestions, setSuggestions] = useState([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
   const isLogin = localStorage.getItem("userId")
